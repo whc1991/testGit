d2a58f463ab2e5427bdebc0d0969771a368f6e1b
wanghc <67023986@qq.com>
2018-06-08 17:00:08 +0800

    修改方法名

--- src/Test1.java
+++ src/Test1.java
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2018/6/8.
  */
-public class Test {
+public class Test1 {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
